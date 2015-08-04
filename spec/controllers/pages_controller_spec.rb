2ab9fe1ecdbb7569e95ff930f796b21705c26162
bivilfd <bivil@freshdesk.com>
2015-08-04 19:40:03 +0530

    spec users

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -5,16 +5,26 @@ require 'spec_helper'
 describe PagesController do
   integrate_views
 
+  before(:each) do
+    @basetitle = "Ruby on Rails Sample App"
+  end
+
   #Delete these examples and add some real ones
  # it "should use PagesController" do
   #  controller.should be_an_instance_of(PagesController)
   #end
 
 
+
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
+    end
+    it "should have the right title" do
+      get 'home'
+      response.should have_tag("title",
+        @basetitle + " | Home")
     end
   end
 
@@ -23,6 +33,11 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+    it "should have the right title" do
+      get 'contact'
+      response.should have_tag("title",
+        @basetitle + " | Contact")
+    end
   end
 
 
@@ -30,6 +45,11 @@ describe PagesController do
     it "should be successful" do
       get 'about'
       response.should be_success
+    end
+    it "should have the right title" do
+      get 'about'
+      response.should have_tag("title",
+        @basetitle + " | About")
     end
   end
 
